17d9b04ecb98dc0cd6e064df54cc8509fedf35f5
Darlyn Carmenate <darlyncarmenate@gmail.com>
2024-01-11 21:04:17 +0300

    Creating, updating and deleting objects

---
+++ src/hibernate/java/Test1.java
@@ -0,0 +1,27 @@
+package hibernate.java;
+
+import hibernate.java.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try{
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Oleg", "Vasnetsov",
+                    "IT", 900);
+            session.beginTransaction();
+            session.persist(emp);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+
+    }
+}
---
+++ src/hibernate/java/Test5.java
@@ -0,0 +1,31 @@
+package hibernate.java;
+
+import hibernate.java.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test5 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try{
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            Employee emp = session.get(Employee.class, 2);
+//            session.remove(emp);
+
+            session.createQuery("delete Employee where name = 'Elena'")
+                            .executeUpdate();
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+        }
+
+    }
+}
