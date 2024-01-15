83fbcc93135cbb2efabc06bb4b2779cfad35df7c
Darlyn Carmenate <darlyncarmenate@gmail.com>
2024-01-15 21:52:51 +0300

    One-to-one relations

---
+++ src/hibernate2/java/Test1.java
@@ -0,0 +1,42 @@
+package hibernate2.java;
+
+import hibernate2.java.entity.Detail;
+import hibernate2.java.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+        try{
+//            Session session = factory.getCurrentSession();
+//            Employee emp = new Employee("Oleg", "Vasnetsov",
+//                    "IT", 900);
+//            Detail detail = new Detail("Placetas", "987866",
+//                    "carmenate@mail.ru");
+//            emp.setEmpDetail(detail);
+//            session.beginTransaction();
+//            session.persist(emp);
+//            session.getTransaction().commit();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class, 1);
+            session.remove(emp);
+
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
---
+++ src/hibernate2/java/Test2.java
@@ -0,0 +1,51 @@
+package hibernate2.java;
+
+import hibernate2.java.entity.Detail;
+import hibernate2.java.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+        try{
+//            session = factory.getCurrentSession();
+//            Employee emp = new Employee("Misha", "Kuznetsov",
+//                    "Sales", 900);
+//            Detail detail = new Detail("Perm", "987856",
+//                    "misha@mail.ru");
+//            emp.setEmpDetail(detail);
+//            detail.setEmployee(emp);
+//            session.beginTransaction();
+//            session.persist(detail);
+//            session.getTransaction().commit();
+
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Detail detail = session.get(Detail.class, 3);
+//            System.out.println(detail.getEmployee());
+//            session.getTransaction().commit();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Detail detail = session.get(Detail.class, 2);
+            detail.getEmployee().setEmpDetail(null);
+            session.remove(detail);
+
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
